d41cb5bb450af3122d13c79976ecd0967e9f2808
Jeffrey Lim <40591354+jefflimts@users.noreply.github.com>
2020-10-30 10:26:32 -0400

    updated cert views

--- app/public/js/views/cert_detail.app.js
+++ app/public/js/views/cert_detail.app.js
@@ -3,8 +3,10 @@ var app = new Vue({
   data: {
     certList: [],
     certDetails: [],
-    mbrsWithCert: [],
-    certSelected: ''
+    mbrsWithCertValid: [],
+    mbrsWithCertExp: [],
+    certSelected: '',
+    expiredViewToggle: false
   },
 
   created() {
@@ -12,18 +14,39 @@ var app = new Vue({
   },
 
   methods: {
+    // fetches certification details and members with it from API
     fetchPageDetails: function(){
+      // gets id from query string to build path to API
       const urlParam = new URLSearchParams(window.location.search);
       this.certSelected = urlParam.get('id');
       var apiPath = '/api/views/cert_detail.php?certId='.concat(this.certSelected);
+
+      // fetches
      fetch(apiPath)
      .then(response => response.json())
      .then(data => {
+       // first item in API endpoint is details of the certification
        this.certDetails = data['cert_details'][0];
-       this.mbrsWithCert.splice(0, this.mbrsWithCert.length);
+
+       // clears lists
+       this.mbrsWithCertValid.splice(0, this.mbrsWithCertValid.length);
+       this.mbrsWithCertExp.splice(0, this.mbrsWithCertExp.length);
+
+      // iterates, pushes to two lists based on status
        for(i = 0; i < data['members'].length; i++){
-         this.mbrsWithCert.push(data['members'][i]);
+         if(data['members'][i]['status'] === 'Expired'){
+           this.mbrsWithCertExp.push(data['members'][i]);
+         }
+         else{
+           this.mbrsWithCertValid.push(data['members'][i])
+         }
        }
+
+       // unhides expired table if there are any members with expired certs
+       if(this.mbrsWithCertExp.length > 0){
+         this.expiredViewToggle = true;
+       }
+
      });
     }
   }
--- app/public/js/views/mbr_detail.app.js
+++ app/public/js/views/mbr_detail.app.js
@@ -3,8 +3,10 @@ var app = new Vue({
   data: {
     mbrList: [],
     mbrDetails: [],
-    mbrCerts: [],
-    mbrSelected: ''
+    mbrValidCerts: [],
+    mbrExpiredCerts: [],
+    mbrSelected: '',
+    expiredViewToggle: false
   },
 
   created() {
@@ -12,17 +14,37 @@ var app = new Vue({
   },
 
   methods: {
+    // fetches member details and certifications from API
     fetchPageDetails: function(){
+      // gets id from query string to build path to API
       const urlParam = new URLSearchParams(window.location.search);
       this.mbrSelected = urlParam.get('id');
       var apiPath = '/api/views/mbr_detail.php?mbrId='.concat(this.mbrSelected);
+
+     // fetches
      fetch(apiPath)
      .then(response => response.json())
      .then(data => {
+       // first item in API endpoint is details of the member
        this.mbrDetails = data['mbr_details'][0];
-       this.mbrCerts.splice(0, this.mbrCerts.length);
+
+       // clears the lists for certs
+       this.mbrValidCerts.splice(0, this.mbrValidCerts.length);
+       this.mbrExpiredCerts.splice(0, this.mbrExpiredCerts.length);
+
+       // iterates, pushes to two lists based on status
        for(i = 0; i < data['certs'].length; i++){
-         this.mbrCerts.push(data['certs'][i]);
+         if(data['certs'][i]['status'] === 'Expired'){
+           this.mbrExpiredCerts.push(data['certs'][i]);
+         }
+         else{
+           this.mbrValidCerts.push(data['certs'][i]);
+         }
+       }
+
+       // unhides expired table if there are any expired cert
+       if(this.mbrExpiredCerts.length > 0){
+         this.expiredViewToggle = true;
        }
      });
     }
