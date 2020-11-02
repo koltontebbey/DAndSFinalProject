0883d95cd9e4891d9204134851ad75358b08f7f7
Jeffrey Lim <40591354+jefflimts@users.noreply.github.com>
2020-11-02 00:28:34 -0500

    func

---
+++ app/public/api/mutate/delete/cert_assoc.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'common.php';
+
+// Get connection from helper
+$db = DbConnection::getConnection();
+
+// check if the certId is set
+if (isset($_POST['certId']) && isset($_POST['personId'])) {
+  /*
+      SQL statement: deletes the association between person and cert
+  */
+  $sql = "DELETE FROM Cert_assoc WHERE cert_id = ? AND person_id = ?";
+  $stmt = $db->prepare($sql);
+  $vars = [$_POST['certId'], $_POST['person_id']];
+  $stmt->execute($vars);
+
+  $statusObj = array('status' => 'ok');
+
+  $json = json_encode($statusObj, JSON_PRETTY_PRINT);
+
+  // 200 OK
+  http_response_code(200);
+  header('Content-Type: application/json');
+
+  echo $json;
+}
+else{
+  // returns 400 bad request error if no param is entered.
+  http_response_code(400);
+}
---
+++ app/public/api/mutate/delete/station_assoc.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'common.php';
+
+// Get connection from helper
+$db = DbConnection::getConnection();
+
+// check if the certId is set
+if (isset($_POST['stationId']) && isset($_POST['personId'])) {
+  /*
+      SQL statement: deletes association between person and station
+  */
+  $sql = "DELETE FROM Station_assoc WHERE station_id = ? AND person_id = ?";
+  $stmt = $db->prepare($sql);
+  $vars = [$_POST['station_id'], $_POST['person_id']];
+  $stmt->execute($vars);
+
+  $statusObj = array('status' => 'ok');
+
+  $json = json_encode($statusObj, JSON_PRETTY_PRINT);
+
+  // 200 OK
+  http_response_code(200);
+  header('Content-Type: application/json');
+
+  echo $json;
+}
+else{
+  // returns 400 bad request error if no param is entered.
+  http_response_code(400);
+}
