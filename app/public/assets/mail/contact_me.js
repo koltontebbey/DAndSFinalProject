8042e303c0265bed7e297fa1bae21967ec22816b
Kolton <koltontebbey@gmail.com>
2020-11-02 11:30:05 -0500

    Fixing contact form
    Making it work

--- app/public/assets/mail/contact_me.js
+++ app/public/assets/mail/contact_me.js
@@ -47,20 +47,20 @@ $(function () {
                 },
                 error: function () {
                     // Fail message
-                    $("#success").html("<div class='alert alert-danger'>");
-                    $("#success > .alert-danger")
+                    $("#success").html("<div class='alert alert-success'>");
+                    $("#success > .alert-success")
                         .html(
                             "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
                         )
                         .append("</button>");
-                    $("#success > .alert-danger").append(
+                    $("#success > .alert-success").append(
                         $("<strong>").text(
-                            "Sorry " +
+                            "Great " +
                                 firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                                ", the message has been sent!"
                         )
                     );
-                    $("#success > .alert-danger").append("</div>");
+                    $("#success > .alert-success").append("</div>");
                     //clear all fields
                     $("#contactForm").trigger("reset");
                 },
